dd9dbb542c1e084a91e58c47998fa7dc3e5d555f
Eder Sena <eder.sena@live.com>
2020-12-04 21:45:26 -0300

    Requerimento 2

--- middlewares/addCrush.js
+++ middlewares/addCrush.js
@@ -4,6 +4,6 @@ module.exports = async (req, res) => {
   const { name, age, date } = req.body;
   const listaAtualCrush = await lerCrush();
   const id = listaAtualCrush.length + 1;
-  adicionaCrush(listaAtualCrush, { name, age, id, date });
+  adicionaCrush(listaAtualCrush, { id, name, age, date });
   res.status(201).json({ id, name, age, date });
 };
