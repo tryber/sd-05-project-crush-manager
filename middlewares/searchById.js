f8faf2d44078841e8b89e2fdbef07b89d46f2706
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-01-27 08:11:12 -0300

    requisito 4 fixing lint

--- middlewares/searchById.js
+++ middlewares/searchById.js
@@ -4,7 +4,7 @@ const searchById = async (req, res) => {
   const file = await fileSystem.readCrushPromise('./crush.json');
   console.log(typeof file);
   const list = JSON.parse(file);
-  const crush = list.find((crush) => parseInt(req.params.id, 10) === crush.id);
+  const crush = list.find((element) => parseInt(req.params.id, 10) === element.id);
   if (crush) {
     res.status(200).send(crush);
   }
