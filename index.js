071ce5fda685e2f69edaa237e7882571b0746f65
Ishidor <kevinmendessilva@hotmail.com>
2021-01-28 06:21:38 -0300

    Teste

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+// const loginRoute = require('./middlewares');
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/login', loginRoute);
